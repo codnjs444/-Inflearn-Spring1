53a6554614b596dd93588427a6ba2508bc61638a
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-09-22 21:49:55 +0900

    study : init

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","hello");
+        return "hello";
+    }
+}
