0158faa083c0078e2ad409712714c1a49fe4bfc6
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-09-27 21:48:10 +0900

    study : service test 작성중

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -3,22 +3,39 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
-
 
     @Test
     void 회원가입() {
         //given
         Member member = new Member();
         member.setName("hello");
+
         //when
         Long saveId = memberService.join(member);
+
         //then
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        //then
     }
 
     @Test
