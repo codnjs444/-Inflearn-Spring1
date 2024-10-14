f37baa1906e509589bf751e8546004cde7cd5bcd
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-15 08:00:40 +0900

    24.10.15 : 모닝 서비스

--- src/main/java/hello/hello_spring/service/SpringConfig.java
+++ src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -2,11 +2,22 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
