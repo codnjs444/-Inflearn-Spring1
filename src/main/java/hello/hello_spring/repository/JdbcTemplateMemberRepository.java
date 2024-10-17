303f3b50583e947a0354d83e8d70376179681dea
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-17 22:06:38 +0900

    ing

---
+++ src/main/java/hello/hello_spring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,50 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return jdbcTemplate.query("select * from member where id = ?");
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return List.of();
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return  (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
