0981b6f21a6463b7c03f42c6451312c287c67790
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-20 11:19:19 +0900

    24.10.20 : AOP 테스트

--- src/main/java/hello/hello_spring/service/SpringConfig.java
+++ src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
@@ -26,6 +27,11 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository(){
